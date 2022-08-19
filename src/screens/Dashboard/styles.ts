13671ee8c09c55054d61eb4a368990d2befda79f
Filipe Simoes <109987257+ArqueoDude@users.noreply.github.com>
2022-08-19 12:14:58 -0300

    dia 1
    inicio

---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+        flex: 1;
+        justify-Content: center;
+        align-Items: center;
+`;
+
+export const Title = styled.Text`
+    font-size: 24px;
+    font-weight: bold;
+    color: rebeccapurple
+`;
